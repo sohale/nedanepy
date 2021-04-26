9867988bf5927f190c6cbd27c3b8b90d99946ca5
Sohail Siadatnejad <sohale@gmail.com>
2021-04-26 13:50:17 +0100

    random_dates.py from  3 Apr

--- random_dates.py
+++ random_dates.py
@@ -2,16 +2,21 @@ import random
 
 # مقایسه ترتیب دو آبجکت تاریخ. برای استفاده در مرتب سازی
 
-def earlier_than(prev, next):
+def earlier_than(prev, next_):
+    """
+      مقایسه ی دو آبجکت (دیکشنری) تاریخ
+      نوصسنده: سهیل
+    """
+    if prev["year"] < next_["year"]:
+        return True
 
-    if prev["year"] < next["year"]:
-      return True
+    if prev["year"] == next_["year"] and prev["month"] < next_["month"]:
+        return True
 
-    elif prev["year"] == next["year"] and prev["month"] < next["month"]:
-      return True
-
-    elif prev["year"] == next["year"] and prev["month"] == next["month"] and prev["day"] < next["day"]:
-      return True
+    if prev["year"] == next_["year"] \
+              and prev["month"] == next_["month"] \
+              and prev["day"] < next_["day"]:
+        return True
 
     return False
 
@@ -28,65 +33,77 @@ def sort_dates_sosi(arr):
      i = i+1
 """
 
-# تولید یک تاریخ رندم
-# نویسنده: ندا
+
 def new_random_date():
-  obj = {}
-  # random.randint(1900,2020)
-  y = random.randint(1000,1001)
-  m = random.randint(1,12)
-  d = random.randint(1,30)
-  obj.update({"year":y});
-  obj.update({"month":m});
-  obj.update({"day":d});
-  return obj
+    """
+      تولید یک تاریخ رندم
+      نویسنده: ندا
+    """
+    obj = {}
+    # random.randint(1900,2020)
+    y = random.randint(1000, 1001)
+    m = random.randint(1, 12)
+    d = random.randint(1, 30)
+    obj.update({"year":y})
+    obj.update({"month":m})
+    obj.update({"day":d})
+    return obj
 
-# تولید آرایه ای پر از تاریخ های رندم
-# نویسنده: ندا
 def create_list_of_dates():
-  i = 0
-  arr = []
-  c = {}
-  while i<7:
-    obj = new_random_date()
-    #print((i+1), obj["year"], obj["month"], obj["day"])
-    arr.append(obj)
-    c.update(obj)
-    #print((i+1),c)
-    i = i+1
-  return arr
+    """
+      تولید آرایه ای پر از تاریخ های رندم
+      نویسنده: ندا
+    """
+    i = 0
+    arr = []
+    c = {}
+    while i < 7:
+        obj = new_random_date()
+        #print((i+1), obj["year"], obj["month"], obj["day"])
+        arr.append(obj)
+        c.update(obj)
+        #print((i+1),c)
+        i = i+1
+    return arr
 
-# مرتب سازی ()
-# نویسنده»: ندا
-def sort_list_of_dates (b):
-  i = 0
-  while i<len(b)-1:
-    #if b.value("year") in b[i]<b.value("year") in b[i+1]:
-    #  if b.value("month") in b[i]<b.value("month") in b[i+1]:
-    #    if b.value("day") in b[i]<b.value("day") in b[i+1]:
-    if  earlier_than(b[i+1], b[i]):
-        t = b[i+1]
-        b[i+1] = b[i]
-        b[i] = t
-        i = -1
+def sort_list_of_dates(b):
+    """
+      مرتب سازی
+      نویسنده: ندا
+    """
+    i = 0
+    while i<len(b)-1:
+        #if b.value("year") in b[i]<b.value("year") in b[i+1]:
+        #  if b.value("month") in b[i]<b.value("month") in b[i+1]:
+        #    if b.value("day") in b[i]<b.value("day") in b[i+1]:
+        if  earlier_than(b[i+1], b[i]):
+            t = b[i+1]
+            b[i+1] = b[i]
+            b[i] = t
+            i = -1
 
-        #if b[i] == b[i+1]:
-        #  b.remove(b[i])
-        #i = 0
-    i = i+1
-  return
+            #if b[i] == b[i+1]:
+            #  b.remove(b[i])
+            #i = 0
+        i = i+1
+    return
 
 def print_list_of_dates(arr):
-  i = 0
-  while i<len(arr):
-    x = arr[i]
-    print(i, x["year"], x["month"], x["day"])
-    i = i+1
+    """
+    آرایه ای که هر عنصرش یک آبجکت (دیکشنری = تاریخ)‌است را چاپ میکند.
+    """
+    i = 0
+    while i<len(arr):
+        x = arr[i]
+        print(i, x["year"], x["month"], x["day"])
+        i = i+1
 
-# بخش اصلی (main) برنامه:
-# بعدش اجرای همه فانکشن ها:
+"""
+  بخش اصلی (main) برنامه:
+  بعدش اجرای همه فانکشن ها:
+"""
 
-arr = create_list_of_dates()
-#sort_dates(arr)
-sort_list_of_dates(arr)
-print_list_of_dates(arr)
+my_date_array = create_list_of_dates()
+#sort_dates(my_date_array)
+sort_list_of_dates(my_date_array)
+print_list_of_dates(my_date_array)
